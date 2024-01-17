b540725cffeb84d9f545850e810f5d2874352c4f
queen-favour <elechifavour3@gmail.com>
2024-01-17 20:50:43 +0100

    footer section completed and responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import { BsLinkedin } from "react-icons/bs"
+import { FiInstagram } from "react-icons/fi"
+import { IoLogoTwitter } from "react-icons/io"
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Queen Favour</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+      <a href="https://www.linkedin.com/in/favour-elechi-9401a7218/" target='_blank'><BsLinkedin /></a>
+        <a href="https://twitter.com/just_QueenFavor" target='_blank'><IoLogoTwitter /></a>
+        <a href="https://www.instagram.com/_queen_favour/" target='_blank'><FiInstagram /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Queen Favour. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ import { BsTwitterX } from "react-icons/bs"
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="www.linkedin.com/in/favour-elechi-9401a7218" target='_blank'><BsLinkedin /></a>
+        <a href="https://www.linkedin.com/in/favour-elechi-9401a7218/" target='_blank'><BsLinkedin /></a>
         <a href="https://twitter.com/just_QueenFavor" target='_blank'><BsTwitterX /></a>
         <a href="https://github.com/queen-favour" target='_blank'><FaGithub /></a>
 
